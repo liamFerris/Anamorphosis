c9f6acf7029149b67fd22f011f396f24a98b53eb
Liam Ferris <L.Ferris@kainos.com>
2017-06-11 14:57:03 +0100

    added movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 v;
+    Vector3 i;
+    bool ready = false;
+
+    void Start()
+    {
+        v = new Vector3(Random.value, Random.value, Random.value);
+        i = transform.position;
+        Invoke("go", 2);
+    }
+
+    void Update()
+    {
+        if (ready)
+            transform.RotateAround(new Vector3(150, 0, 0), v, 0.75f);
+        if (ready && transform.position == i)
+        {
+            ready = false;
+            Invoke("go", 2);
+        }
+    }
+
+    void go()
+    {
+        ready = true;
+    }
+}
